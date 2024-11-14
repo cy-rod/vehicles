701b28750afbe3c7ba8a3dd8f3d9cb496add1128
cy-rod <cyril.l.rodriguez@gmail.com>
2024-11-13 16:29:44 -0800

    1. EDA - added title and intro 2. EDA - added intermediate and overall conclusions 3. changed st.write to st.caption 4. added title for each chart 5. added customizable title for the last chart

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.image(image, use_container_width=True)
 # Page Description
 st.write("""
 # USED VEHICLE SALES
-This *interactive page* shows the statistics of used vehicles sales base on different variables. Move the cursor to any data point on any charts for more information.
+This *interactive page* shows the statistics of used vehicle sales base on different variables. Move the cursor to any data point on any charts for more information.
 ***        
 """)
 
@@ -39,8 +39,9 @@ vehicles = pd.read_csv('vehicles_us.csv')
 # Add a 'manufacturer' column in the dataset for easier reference
 vehicles['manufacturer'] = vehicles['model'].apply(lambda x: x.split()[0])
 
-# Add header
+# Add header and title
 st.header('View Dataset')
+st.caption('User can search, expand or download the data as CSV.')
 
 # Display dataframe on streamlit
 st.dataframe(vehicles)
@@ -54,10 +55,10 @@ st.write("""
 
 # Add header and "Tick box" instruction
 st.header('Vehicle Condition Per Manufacturer')
-st.write("""Tick/Untick the boxes on the right to sort by vehicle condition. Double click to isolate.""")
+st.caption('Tick/Untick boxes on the right to sort, double click to isolate. User can also download plot as an image')
 
 # Plot the histogram distribution of vehicle's condition by the manufacturer
-fig = px.histogram(vehicles, x="manufacturer", color="condition", height=600, 
+fig = px.histogram(vehicles, x="manufacturer", color="condition", height=600, title='Amount of Vehicles Sold by Brand, Sorted by Vehicle Condition', 
                   category_orders={"condition": ["new", "like new", "excellent", "good", "fair", "salvage"]})
 
 # Sort category alphabetically and change axes and legend names
@@ -82,10 +83,10 @@ st.write("""
 
 # Add header and "Tick box" instruction
 st.header('Fuel Type Per Vehicle Type')
-st.write("""Tick/Untick the boxes on the right to sort by fuel type. Double click to isolate.""")
+st.caption('Tick/Untick boxes on the right to sort, double click to isolate. User can also download plot as an image')
 
 # Plot the distribution of fuel type per vehicle type
-fig = px.histogram(vehicles, x="type", color="fuel", height=600,
+fig = px.histogram(vehicles, x="type", color="fuel", height=600, title='Amount of Vehicles Sold by Vehicle Type, Sorted by Fuel Type',
                   category_orders={"fuel": ["gas", "diesel", "electric", "hybrid", "other"]})
 
 # Sort category alphabetically and change axes and legend names
@@ -109,14 +110,15 @@ st.write("""
 ##### HISTOGRAM 3 #####
 
 # Add header and "Tick box" instruction
-st.header('Distribution of Vehicle Type Per Year Model')
-st.write("""Tick/Untick the boxes on the right to sort by vehicle type. Double click to isolate.""")
+st.header('Vehicle Type Per Year Model')
+st.caption('Tick/Untick boxes on the right to sort, double click to isolate. User can also download plot as an image')
 
 # Make a sorted list of vehicle type
 type_list = sorted(vehicles['type'].unique())
 
 # Plot distribution of vehicle type by the year model
 fig = px.histogram(vehicles, x="model_year", color="type", height=700,
+                  title='Distribution of the Amount of Vehicles Sold per Year Model, Sorted by Vehicle Type',
                   category_orders={"type": type_list})
 
 # Change axes and legend names
@@ -139,14 +141,15 @@ st.write("""
 ##### HISTOGRAM 4 #####
 
 # Add header and "Tick box" instruction
-st.header('Distribution of Manufacturer Per Year Model')
-st.write("""Tick/Untick the dots on the right to sort by vehicle manufacturer. Double click to isolate.""")
+st.header('Manufacturer Per Year Model')
+st.caption('Tick/Untick the dots on the right to sort, double click to isolate. User can also download plot as an image')
 
 # Make a sorted list of vehicle manufacturers
 manufac_list = sorted(vehicles['manufacturer'].unique())
 
 # Plot distribution between vehicle manufacturer and the year model
 fig = px.scatter(vehicles, x='model_year', color='manufacturer', height=900,
+                title='Distribution Index of Vehicles Sold per Year Model, Sorted by Manufacturer',
                 category_orders={"manufacturer": manufac_list})
 
 # Change axes and legend names
@@ -170,7 +173,7 @@ st.write("""
 
 # Add header and "Tick box" instruction
 st.header('Compare Price Distribution of Two Manufacturers Base on Vehicle Type')
-st.write("""Tick/Untick the boxes on the right to sort by vehicle manufacturer. Double click to isolate.""")
+st.caption('Tick/Untick boxes on the right to sort, double click to isolate. User can also download plot as an image')
 
 # Create a box with dropdown menu for user to select vehicle type
 # Use the sorted list of vehicle manufacturers and vehicle types above
@@ -183,10 +186,16 @@ manufacturer_sel = st.selectbox(label='Compare (Select Manufacturer)',
                                 options=manufac_list, 
                                 index=manufac_list.index('toyota'))
 
+# Capitalize first manufacturer selected
+manuf_1 = manufacturer_sel.capitalize()
+
 # Create a box with dropdown menu for user to select second vehicle manufacturer
 manufacturer_sel2 = st.selectbox(label='With (Select Manufacturer)',
                                 options=manufac_list, 
                                 index=manufac_list.index('honda'))
+
+# Capitalize second manufacturer selected
+manuf_2 = manufacturer_sel2.capitalize()
 
 # Filter dataframe using boolean conditions and save it as a new dataframe 
 boolean_cond = (vehicles['type'] == type_sel) & (vehicles['manufacturer'] == manufacturer_sel) | (vehicles['manufacturer'] == manufacturer_sel2)
@@ -194,6 +203,7 @@ vehicles_filtered = vehicles[boolean_cond]
 
 # Create a histogram as the main (default) chart
 fig = px.histogram(vehicles_filtered,
+                    title=f'Comparing {manuf_1} and {manuf_2}',
                     x='price',
                     nbins=30,
                     color='manufacturer',
@@ -203,6 +213,7 @@ fig = px.histogram(vehicles_filtered,
 
 # Create histogram subplots base on vehicle condition
 fig_facet = px.histogram(vehicles_filtered,
+                    title=f'Comparing {manuf_1} and {manuf_2} Subplotted by Vehicle Condition',
                     x='price',
                     nbins=30,
                     color='manufacturer',
